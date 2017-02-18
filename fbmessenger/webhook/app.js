8538a879be248eceef3991b985aa79f25fa637a8
Binit Ghimire <thebinitghimire@gmail.com>
2017-02-18 22:01:47 +0545

    Update and rename index.js to app.js

--- fbmessenger/webhook/app.js
+++ fbmessenger/webhook/app.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'my_voice_is_not_my_password_verify_me') {
+      req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
