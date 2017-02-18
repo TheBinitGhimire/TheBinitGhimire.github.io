2fe5375c9e3d4c8b8a000ed408b084049d6828ae
Binit Ghimire <thebinitghimire@gmail.com>
2017-02-18 21:42:04 +0545

    Update index.js

--- fbmessenger/webhook/index.js
+++ fbmessenger/webhook/index.js
@@ -1,3 +1,18 @@
+var express = require('express');  
+var bodyParser = require('body-parser');  
+var request = require('request');  
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));  
+app.use(bodyParser.json());  
+app.listen((process.env.PORT || 3000));
+
+// Server frontpage
+app.get('/', function (req, res) {  
+    res.send('This is Binit Ghimire Chat Bot');
+});
+
+
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === 'my_voice_is_not_my_password_verify_me') {
